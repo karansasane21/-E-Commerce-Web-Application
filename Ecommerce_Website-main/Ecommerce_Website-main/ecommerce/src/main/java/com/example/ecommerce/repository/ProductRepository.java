f96d1da7b8b4e44c6ec1130b7b01d9200aaf542d
Karan Sasane <135867618+karansasane21@users.noreply.github.com>
2026-09-24 18:34:27 +0530

    Delete Ecommerce_Website-main/Ecommerce_Website-main directory

--- Ecommerce_Website-main/Ecommerce_Website-main/ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ecommerce.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
